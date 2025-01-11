0da7f5b25bd705c16e907afc721f85fa61ccae03
Kzsanjeet <sanjeetkazithapa@gmail.com>
2025-01-11 18:00:43 +0545

    updated package json

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,22 @@ import express, { Request, Response, urlencoded } from "express";
 import dotenv from "dotenv";
 dotenv.config()
 
-import addTourRouter from "./routes/tours.routes/addtours.routes";
-import signupRouter from "./routes/login&signup/signupRouter";
-import loginRouter from "./routes/login&signup/loginRoutes";
+// import addTourRouter from "./routes/tours.routes/addtours.routes";
+// import signupRouter from "./routes/login&signup/signupRouter";
+// import loginRouter from "./routes/login&signup/loginRoutes";
 import cookieParser from "cookie-parser";
-import otpRouter from "./routes/otp/otpRoutes";
-import getActivateTourRouter from "./routes/tours.routes/getActivateTours.routes";
-import getAllTourRouter from "./routes/tours.routes/getAllTours.routes";
-import activeteTourRouter from "./routes/tours.routes/activateTours.routes";
-import getRecommendTourRouter from "./routes/tours.routes/getRecommendTours.routes";
-import recommendTourRouter from "./routes/tours.routes/recommendTours.routes";
-import dbConnection from "./connectDB";
+// import otpRouter from "./routes/otp/otpRoutes";
+// import getActivateTourRouter from "./routes/tours.routes/getActivateTours.routes";
+// import getAllTourRouter from "./routes/tours.routes/getAllTours.routes";
+// import activeteTourRouter from "./routes/tours.routes/activateTours.routes";
+// import getRecommendTourRouter from "./routes/tours.routes/getRecommendTours.routes";
+// import recommendTourRouter from "./routes/tours.routes/recommendTours.routes";
+// import dbConnection from "./connectDB";
 
 const app = express();
 
 // Connect to database
-dbConnection();
+// dbConnection();
 
 const port = process.env.PORT || 4000;  // Use PORT from .env or default to 4000
 
@@ -26,20 +26,20 @@ app.use(urlencoded({extended:true}))
 app.use(cookieParser())
 
 //for client route
-app.use(
-    getActivateTourRouter,
-    getRecommendTourRouter
-)
+// app.use(
+//     getActivateTourRouter,
+//     getRecommendTourRouter
+// )
 //for adminpanel route
-app.use("/admin",
-    signupRouter,
-    loginRouter,
-    otpRouter,
-    addTourRouter,
-    getAllTourRouter,
-    activeteTourRouter,
-    recommendTourRouter
-)
+// app.use("/admin",
+//     signupRouter,
+//     loginRouter,
+//     otpRouter,
+//     addTourRouter,
+//     getAllTourRouter,
+//     activeteTourRouter,
+//     recommendTourRouter
+// )
 
 
 

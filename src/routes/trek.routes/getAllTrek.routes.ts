d3c48acab25c540ec8b599fe04bc1a05f178622e
Kzsanjeet <sanjeetkazithapa@gmail.com>
2025-03-01 16:59:51 +0545

    check

--- src/routes/trek.routes/getAllTrek.routes.ts
+++ src/routes/trek.routes/getAllTrek.routes.ts
@@ -6,5 +6,6 @@ import getAllTreks from "../../controllers/treks/getAllTrek.controller.js";
 const getAllTrekRouter = express.Router();
 
 getAllTrekRouter.route("/trek/get-all").get(getAllTreks);
+// get all 
 
 export default getAllTrekRouter;

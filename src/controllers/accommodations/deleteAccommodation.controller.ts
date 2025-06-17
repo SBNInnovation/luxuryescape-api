7d078290d229301e0b4066deec0aaed1af377e01
Kzsanjeet <sanjeetkazithapa@gmail.com>
2025-06-18 00:17:24 +0545

    added fine dining

--- src/controllers/accommodations/deleteAccommodation.controller.ts
+++ src/controllers/accommodations/deleteAccommodation.controller.ts
@@ -1,33 +1,3 @@
-// import { Request, Response } from "express";
-// import Accommodation from "../../models/accommodation.models/Accommodation.js";
-
-// const deleteAccommodation = async(req:Request, res:Response):Promise<void> =>{
-//     try {
-//         const accommodationId = req.params.accommodationId;
-//         if(!accommodationId){
-//             res.status(400).json({success:false, message: "Accommodation ID is required"});
-//             return
-//         }
-
-//         const deleteAcco = await Accommodation.findByIdAndDelete(accommodationId);
-//         if(!deleteAcco){
-//             res.status(404).json({success:false, message: "Accommodation not found"})
-//             return
-//         }
-//         res.status(200).json({success:true, message: "Accommodation deleted successfully"})
-
-//     } catch (error) {
-//         console.log(error)
-//         if(error instanceof(Error)){
-//             res.status(500).json({success:false, message: error.message})
-//             return
-//         }
-//     }
-// }
-
-// export default deleteAccommodation;
-
-
 import { Request, Response } from "express";
 import Accommodation from "../../models/accommodation.models/Accommodation.js";
 import Tour from "../../models/tours.models/tours.js";
@@ -60,19 +30,6 @@ const deleteAccommodation = async (req: Request, res: Response): Promise<void> =
 
     await Room.updateMany({accommodations: accommodationId}, {$pull:{accommodations: accommodationId }});
 
-    // Delete all accommodation images from Cloudinary
-    // if (accommodation.accommodationPics && accommodation.accommodationPics.length > 0) {
-    //   for (const image of accommodation.accommodationPics) {
-    //     const imagePublicId = image.split('/').pop()?.split('.')[0]; // Extract public ID from the URL
-    //     if (imagePublicId) {
-    //       const deleteResult = await deleteFile(imagePublicId);
-    //       if (!deleteResult) {
-    //         res.status(500).json({ success: false, message: "Failed to delete some images from Cloudinary" });
-    //         return;
-    //       }
-    //     }
-    //   }
-    // }
 
     // const deletedAccommodationPics = await deleteImageGroup(accommodation.accommodationPics, "tours/accommodation/images");
      if (accommodation && accommodation.accommodationPics && accommodation.accommodationPics.length > 0) {
